703e2906c74d593ac6d41089dfb987b2abfe756c
Thyon3 <asnakemengesha80@gmail.com>
2026-01-05 03:48:08 -0500

    feat: assemble landing page with all components

---
+++ onenil-clone/app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import SelectedWorks from "@/components/SelectedWorks";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <SelectedWorks />
+      <Footer />
+    </main>
+  );
+}
